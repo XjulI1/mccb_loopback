1edf41b88d0240ea784a60b1884400ce02a263c9
Xavier Julien <xjulien@octo.com>
2018-10-27 00:40:22 +0200

    Fix lint

--- common/daemon/n26.js
+++ common/daemon/n26.js
@@ -1,62 +1,63 @@
+'use strict';
+
 const config = require('./config/n26');
 const api = require('./config/api');
 let N26 = require('n26');
 let request = require('request-promise');
 
 module.exports = {
-    startDaemon: () => {
-        const minuteTomillisecond = 60 * 1000;
-        const delay = config.n26.minuteDelay * minuteTomillisecond;
+  startDaemon: () => {
+    const minuteTomillisecond = 60 * 1000;
+    const delay = config.n26.minuteDelay * minuteTomillisecond;
 
-        setInterval(importTransactions, delay);
+    setInterval(importTransactions, delay);
 
-        function importTransactions() {
-            let login = config.n26.login,
-                password = config.n26.password,
-                from = Date.now() - delay,
-                to = Date.now();
+    function importTransactions() {
+      let login = config.n26.login;
+      let password = config.n26.password;
+      let from = Date.now() - delay;
+      let to = Date.now();
 
-
-            new N26(login, password)
-                .then(account => account.transactions({from: from, to: to})
-                    .then(transactions => postTransactions(transactions)))
-                .catch((err) => {
-                    console.debug(err);
-                })
-        }
-
-        function postTransactions(transactions) {
-            if (transactions.length === 0) {
-                return true;
-            }
-
-            const transaction = transactions.shift();
-
-            const apiOptions = {
-                method: 'POST',
-                uri: api.protocol + '://' + api.host + ':' + api.port + '/api/Operations',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                },
-                body: {
-                    "NomOp": transaction.merchantName || transaction.partnerName + ' - ' + transaction.referenceText,
-                    "MontantOp": transaction.amount,
-                    "DateOp": (new Date(transaction.visibleTS)).toISOString(),
-                    "CheckOp": true,
-                    "IDcompte": api.IDcompte,
-                    "IDcat": api.IDcat
-                },
-                json: true // Automatically stringifies the body to JSON
-            };
-
-            return request(apiOptions)
-                .then(() => {
-                    return postTransactions(transactions);
-                })
-                .catch((err) => {
-                    console.debug(err);
-                });
-        }
+      new N26(login, password)
+        .then(account => account.transactions({from: from, to: to})
+          .then(transactions => postTransactions(transactions)))
+        .catch((err) => {
+          console.debug(err);
+        });
     }
+
+    function postTransactions(transactions) {
+      if (transactions.length === 0) {
+        return true;
+      }
+
+      const transaction = transactions.shift();
+
+      const apiOptions = {
+        method: 'POST',
+        uri: api.protocol + '://' + api.host + ':' + api.port + '/api/Operations',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: {
+          'NomOp': transaction.merchantName || transaction.partnerName + ' - ' + transaction.referenceText,
+          'MontantOp': transaction.amount,
+          'DateOp': (new Date(transaction.visibleTS)).toISOString(),
+          'CheckOp': true,
+          'IDcompte': api.IDcompte,
+          'IDcat': api.IDcat,
+        },
+        json: true, // Automatically stringifies the body to JSON
+      };
+
+      return request(apiOptions)
+        .then(() => {
+          return postTransactions(transactions);
+        })
+        .catch((err) => {
+          console.debug(err);
+        });
+    }
+  },
 };
--- common/models/operation.js
+++ common/models/operation.js
@@ -1,24 +1,31 @@
-'use strict'
+'use strict';
 
-module.exports = function (Operation) {
-  Operation.sumForACompte = function (CompteID, cb) {
-    const sqlChecked = 'SELECT IDCompte, SUM(MontantOp) as TotalChecked FROM Operation WHERE IDcompte = ' + CompteID + ' AND CheckOp = true GROUP BY IDCompte'
-    const sqlNotChecked = 'SELECT IDCompte, SUM(MontantOp) as TotalNotChecked FROM Operation WHERE IDcompte = ' + CompteID + ' AND CheckOp = false GROUP BY IDCompte'
+module.exports = function(Operation) {
+  Operation.sumForACompte = function(CompteID, cb) {
+    const sqlChecked = 'SELECT IDCompte, SUM(MontantOp) as TotalChecked ' +
+      'FROM Operation ' +
+      'WHERE IDcompte = ' + CompteID + ' AND CheckOp = true ' +
+      'GROUP BY IDCompte';
+
+    const sqlNotChecked = 'SELECT IDCompte, SUM(MontantOp) as TotalNotChecked ' +
+      'FROM Operation ' +
+      'WHERE IDcompte = ' + CompteID + ' AND CheckOp = false ' +
+      'GROUP BY IDCompte';
 
     Operation.dataSource.connector.executeSQL(sqlChecked, [], [], (err, data) => {
-      const checkedTotal = data
+      const checkedTotal = data;
 
       Operation.dataSource.connector.executeSQL(sqlNotChecked, [], [], (err, data) => {
-        cb(null, { ...checkedTotal[0], ...data[0] })
-      })
-    })
-  }
+        cb(null, Object.assign(checkedTotal[0], data[0]));
+      });
+    });
+  };
 
   Operation.remoteMethod('sumForACompte', {
-    accepts: { arg: 'id', type: 'number' },
-    returns: { arg: 'results', type: 'object' },
+    accepts: {arg: 'id', type: 'number'},
+    returns: {arg: 'results', type: 'object'},
     http: {
-      verb: 'get'
-    }
-  })
-}
+      verb: 'get',
+    },
+  });
+};
