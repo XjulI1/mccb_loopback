f1cb4ff2171af994915ca71f32b62b1f9e0e7342
Xavier Julien <xjulien@octo.com>
2018-10-27 00:02:37 +0200

    Create sumForACompte function on Operation model

--- common/models/operation.js
+++ common/models/operation.js
@@ -1,5 +1,24 @@
-'use strict';
+'use strict'
 
-module.exports = function(Operation) {
+module.exports = function (Operation) {
+  Operation.sumForACompte = function (CompteID, cb) {
+    const sqlChecked = 'SELECT IDCompte, SUM(MontantOp) as TotalChecked FROM Operation WHERE IDcompte = ' + CompteID + ' AND CheckOp = true GROUP BY IDCompte'
+    const sqlNotChecked = 'SELECT IDCompte, SUM(MontantOp) as TotalNotChecked FROM Operation WHERE IDcompte = ' + CompteID + ' AND CheckOp = false GROUP BY IDCompte'
 
-};
+    Operation.dataSource.connector.executeSQL(sqlChecked, [], [], (err, data) => {
+      const checkedTotal = data
+
+      Operation.dataSource.connector.executeSQL(sqlNotChecked, [], [], (err, data) => {
+        cb(null, { ...checkedTotal[0], ...data[0] })
+      })
+    })
+  }
+
+  Operation.remoteMethod('sumForACompte', {
+    accepts: { arg: 'id', type: 'number' },
+    returns: { arg: 'results', type: 'object' },
+    http: {
+      verb: 'get'
+    }
+  })
+}
