775f6164f2c3fc5513f4d17db6bd40e50ebb17d4
Xavier Julien <xjulien@octo.com>
2019-05-27 15:55:58 +0200

    Update stats endpoints with consolidate solde

--- common/models/stats.js
+++ common/models/stats.js
@@ -2,6 +2,10 @@
 
 module.exports = function (Stats) {
   Stats.evolutionSolde = function (UserID, cb) {
+    const querySoldeTotal = 'SELECT ROUND(SUM(solde), 2) AS sum FROM Compte WHERE IDuser = ' + UserID
+
+    const querySoldeDispo = 'SELECT ROUND(SUM(solde), 2) AS sum FROM Compte WHERE IDuser = ' + UserID + ' AND bloque = 0'
+
     const queryTotal = '' +
       'SELECT ROUND(SUM(MontantOp),2) AS montant, ' +
       'DATE_FORMAT(DateOp, \'%Y-%m-%dT00:00:00.000Z\') AS date ' +
@@ -14,15 +18,21 @@ module.exports = function (Stats) {
       'SELECT ROUND(SUM(MontantOp),2) AS montant, ' +
       'DATE_FORMAT(DateOp, \'%Y-%m-%dT00:00:00.000Z\') AS date ' +
       'FROM Operation NATURAL JOIN Compte ' +
-      'WHERE IDuser = ' + UserID + ' AND bloque = 0' +
+      'WHERE IDuser = ' + UserID + ' AND bloque = 0 ' +
       'GROUP BY date ' +
       'ORDER BY DateOp ASC'
 
-    Stats.dataSource.connector.executeSQL(queryTotal, [], [], (err, dataTotal) => {
-      Stats.dataSource.connector.executeSQL(queryTotal, [], [], (err, dataDispo) => {
-        cb(null, {
-          total: dataTotal,
-          dispo: dataDispo
+    Stats.dataSource.connector.executeSQL(querySoldeTotal, [], [], (err, soldeTotal) => {
+      Stats.dataSource.connector.executeSQL(querySoldeDispo, [], [], (err, soldeDispo) => {
+        Stats.dataSource.connector.executeSQL(queryTotal, [], [], (err, dataTotal) => {
+          Stats.dataSource.connector.executeSQL(queryDispo, [], [], (err, dataDispo) => {
+            cb(null, {
+              soldeTotal: soldeTotal[0].sum,
+              soldeDispo: soldeDispo[0].sum,
+              total: dataTotal,
+              dispo: dataDispo
+            })
+          })
         })
       })
     })
